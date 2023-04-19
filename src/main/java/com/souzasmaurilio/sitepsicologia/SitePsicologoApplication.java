45b8956a8302d09015ae4a8bb66cfaf363085e79
Maurilio Souza <maurilio.ssouza@outlook.com>
2023-04-19 11:35:39 -0300

    criação do projeto site psicólogo

---
+++ src/main/java/com/souzasmaurilio/sitepsicologia/SitePsicologoApplication.java
@@ -0,0 +1,13 @@
+package com.souzasmaurilio.sitepsicologia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitePsicologoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitePsicologoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/souzasmaurilio/sitepsicologia/SitePsicologoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.souzasmaurilio.sitepsicologia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitePsicologoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
